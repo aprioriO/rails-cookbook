bdaa05b40565f161a5b8ac4c18a3b071c8412264
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-13 16:26:25 +0000

    Destroy action outside the nest

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
  root "categories#index"
  resources :categories, only: [:index, :show, :new, :create] do
-  resources :bookmarks, only: [:new, :create, :delete]
+  resources :bookmarks, only: [:new, :create]
  end
+ resources :bookmarks, only: [:destroy]
 end
