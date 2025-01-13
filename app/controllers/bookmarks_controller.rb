1da4e2a3a1726be059e6d7f58f5429ca60b51531
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-13 17:53:11 +0000

    Bookmarked recepies. Working solution. Require refactoring and navigation

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,26 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_category, only: [:new, :create]
+  # before_action :set_bookmark, only: :destroy
+  # before_action :set_category, only: [:new, :create]
 
   def new
+    @category = Category.find(params[:category_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @category = Category.find(params[:category_id])
     @bookmark.category = @category
     if @bookmark.save
       redirect_to category_path(@category)
     else
-      @recipe = Recipe.new
+      # @recipe = Recipe.new
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to category_path(@bookmark.category), status: :see_other
   end
@@ -25,14 +28,14 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :recipe_id)
   end
 
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
+  # def set_bookmark
+  #   @bookmark = Bookmark.find(params[:id])
+  # end
 
-  def set_category
-    @category = Category.find(params[:category_id])
-  end
+  # def set_category
+  #   @category = Category.find(params[:category_id])
+  # end
 end
