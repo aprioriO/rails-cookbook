5f1cac9b0c42df5f6456da6e6753666983a5eddd
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-13 15:52:25 +0000

    Bookmarks conroller and view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,38 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_category, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.category = @category
+    if @bookmark.save
+      redirect_to category_path(@category)
+    else
+      @recipe = Recipe.new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to category_path(@bookmark.category), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
 end
