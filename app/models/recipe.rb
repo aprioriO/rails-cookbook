20a878cbbac53006dc9ecbf2818de9608476deaf
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-13 11:14:45 +0000

    validations and bug fix

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   has_many  :bookmarks
-  has_many  :categories
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
