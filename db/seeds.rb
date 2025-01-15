07068f0fd5380d012d464e09528361992627f092
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-15 22:57:09 +0000

    Fix in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,21 +24,21 @@ puts "Creating new recipes and categories..."
 Recipe.create!(
   name: "Coconut Macaroons",
   description: "These chewy coconut macaroons are very simple to make with flaked coconut and easy ingredients you're sure to have at hand. This recipe has won many first-place blue ribbons at my state fair!",
-  image_url: "https://images.unsplash.com/photo-1634560604992-7784a29bc419?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  image_url: "https://images.unsplash.com/photo-1582189336981-104feabcbf93?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   rating: "3.3"
 )
 
 Recipe.create!(
   name: "Oat meal Macarons",
   description: "This chocolate macaroon recipe is one of our Christmas favorites for no-fuss, no-bake, coconut-oatmeal-chocolate cookies. You can also freeze these. Enjoy!",
-  image_url: "https://images.unsplash.com/photo-1497393001221-abab69e074d1?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  image_url: "https://images.unsplash.com/photo-1596241913248-b344b37931b0?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   rating: "2.8"
 )
 
 Recipe.create!(
   name: "Fruits macaron",
   description: "Macarons are dainty sandwich cookies made with almond flour and meringue and filled with jam, buttercream, or ganache. ",
-  image_url: "https://images.unsplash.com/photo-1692106913324-ef53efa2cd56?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  image_url: "https://images.unsplash.com/photo-1528967425505-cf579e5792d0?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   rating: "4.3"
 )
 
@@ -64,6 +64,7 @@ def recipe_builder(id)
   )
 end
 
+meals_categories = ["Dessert"]
 meals_categories.each do |meal_category|
   url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{meal_category}"
   recipes_serialized = URI.parse(url).read
@@ -99,7 +100,4 @@ dessert6 = Category.create!(name: "Doughnuts")
 recipe6 = Recipe.all[0]
 Bookmark.create!(comment: "Sweeeet!", recipe: recipe6, category: dessert6)
 
-meals_categories = ["Dessert"]
-
-
-puts "Done! Created #{Recipe.all} recipes!"
+puts "Done! Created #{Recipe.count} recipes!"
